a4851df0477133a368ba5bf88b867cdcad37d07b
BFunk <bret.e.funk@gmail.com>
2017-07-25 16:45:05 -0600

    working item model test and updated routes for item show and index pages

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price) }
+  end
+  describe "associations" do
+    it { is_expected.to belong_to(:category) }
+  end
 end
