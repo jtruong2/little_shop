4c6fdc31867e610e7793f24d854549eb9e18baad
wratterman <will.ratterman@gmail.com>
2017-07-25 18:21:32 -0600

    category show pages shows all items associated with it and url reflects that category's name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :items
+
+  def to_param
+    name
+  end
+
+  def self.find_by_param(input)
+    find_by_name(input)
+  end
 end
