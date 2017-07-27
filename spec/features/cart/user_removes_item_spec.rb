8e5a8fcf2fb9468e8c7b0f7f89cadb769902c1af
BFunk <bret.e.funk@gmail.com>
2017-07-27 12:01:08 -0600

    added navbar and jumbotron headers

--- spec/features/cart/user_removes_item_spec.rb
+++ spec/features/cart/user_removes_item_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "As a guest" do
   scenario "removes an item from their cart" do
     item = create(:item)
-    #guest = create(:user, role: 0)
     visit '/menu'
 
     click_on("Add Item")
@@ -11,7 +10,6 @@ RSpec.describe "As a guest" do
     visit '/cart'
 
     click_on "Remove", :match => :first
-    # expect(current_path).to eq(cart_path)
     expect(page).to have_content("Successfully removed #{item.title} from your cart.")
     expect(page).to have_link("#{item.title}")
     expect(page).to_not have_content(item.description)
