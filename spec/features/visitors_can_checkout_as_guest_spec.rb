d299a6c456962c0d0b06c082bf16a1d47b718a81
jtruong2 <jtruong2@comcast.net>
2017-07-29 10:13:31 -0600

    finish story_10

---
+++ spec/features/visitors_can_checkout_as_guest_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+RSpec.describe "Visitor adds items to cart" do
+  # scenario "checks out as guest" do
+  #   item = create(:item)
+  #
+  #   visit items_path
+  #   first(:button, "Add Item").click
+  #   click_on "View Cart"
+  #   click_on "Checkout as guest"
+  #
+  #   expect(page).to have_content("Enter information")
+  # end
+  #
+  scenario "creates an account and checks out as logged in user" do
+    user = create(:user)
+    item = create(:item)
+
+    visit items_path
+    first(:button, "Add Item").click
+    click_on "View Cart"
+
+    expect(page).to have_content(item.title)
+
+    click_on "Create Account"
+    fill_in("user[email]", with: user.email)
+    fill_in("user[password]", with: user.password)
+    fill_in("user[address]", with: user.address)
+    fill_in("user[city]", with: user.city)
+    fill_in("user[state]", with: user.state)
+    fill_in("user[zipcode]", with: user.zipcode)
+    fill_in("user[phone]", with: user.phone)
+    click_on "Register"
+    click_on "View Cart"
+
+    expect(page).to have_content(item.title)
+  end
+end
