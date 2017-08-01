20ae352e4b0271d4827409f6536738617b5d7cd5
erose357 <erose357@gmail.com>
2017-08-01 15:16:13 -0600

    Passing all story 18 tests

--- spec/features/admin/admin_visits_dashboard_spec.rb
+++ spec/features/admin/admin_visits_dashboard_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Admin visits admin dashboard" do
-  it "sees admin dashboard" do
+RSpec.describe "Admin dashboard" do
+  it "admin sees admin dashboard" do
     admin = create(:user, role: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
@@ -10,4 +10,22 @@ RSpec.describe "Admin visits admin dashboard" do
 
     expect(page).to have_content("Admin Dashboard")
   end
+
+  it "users sees 404 page" do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit admin_dashboard_path
+
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
+
+  it "visitor sees 404 page" do
+
+    visit admin_dashboard_path
+
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
+  
 end
