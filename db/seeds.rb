14667e5fcfc8d6bd7a9bbb57dca01261e1077ca1
jtruong2 <jtruong2@comcast.net>
2017-08-01 12:10:08 -0600

    build search

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ IMAGES = ["cheese.png", "meat-lovers.png", "veggie.png", "supreme.png", "peppero
 CATEGORIES.each do |name|
 category = Category.create(name: name)
 2.times do
-  category.items.create(title: ITEMS.sample, price: "9.99", image: IMAGES.sample, description: "YUMMM")
+  category.items.create(title: ITEMS.sample, price: "9.99", image: IMAGES.sample, description: "I love cheese, especially edam taleggio. Cheese and wine manchego goat bavarian bergkase manchego cheese slices cheeseburger dolcelatte. Cheesecake st. agur blue cheese fondue pecorino rubber cheese fromage cheese and biscuits cheesy grin. Dolcelatte.")
  end
 puts "________________Do I was going to say string inteporlation________________"
 end
