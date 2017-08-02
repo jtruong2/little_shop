b57237b110b17b709e1e95b48475b5550568578f
jtruong2 <jtruong2@comcast.net>
2017-08-02 17:05:14 -0600

    push to heroku

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by_param(params[:id])
     @items = @category.items
+    @category_dropdown = Category.all
   end
 end
